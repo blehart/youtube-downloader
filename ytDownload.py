bfa733bfb70d2cb4e5d97da99d54b12177aed369
Robert Hull <robertthull23@gmail.com>
2018-11-08 20:49:29 -0600

    switches from using threading, to use ThreadPoolExecutor

--- ytDownload.py
+++ ytDownload.py
@@ -4,25 +4,31 @@ import sys
 import pafy
 import threading
 import argparse
+from concurrent.futures import ThreadPoolExecutor
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', nargs='*')
 parser.add_argument('-a', '--audio', action='store_true')
 
 def download(urls=[], video=True):
+    executor = ThreadPoolExecutor(max_workers=8)
     for url in urls:
         if 'playlist' in url:
             playlist = pafy.get_playlist(url)
             for item in playlist['items']:
-                threading.Thread(target=downloadBest, args=(item['pafy'].videoid, video)).start()
+                executor.submit(downloadBest, item['pafy'].videoid, video)
+                #pool.apply_async(downloadBest, (item['pafy'].videoid, video))
+                # threading.Thread(target=downloadBest, args=(item['pafy'].videoid, video)).start()
         else:
-            threading.Thread(target=downloadBest, args=(url, video)).start() 
+            executor.submit(downloadBest, url, video)
+            #pool.apply_async(downloadBest, (url, video))
+            # threading.Thread(target=downloadBest, args=(url, video)).start() 
         
 def downloadBest(url, video=True):
     meta = pafy.new(url)
     if video: file = meta.getbest()
     else: file = meta.getbestaudio(preftype="m4a")
-    file.download()
+    file.download(quiet=True)
     print("Finished: ", meta.title, "\t\t\t\t\t")
     
 if __name__ == "__main__":
