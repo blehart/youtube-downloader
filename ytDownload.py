1a83680037fc4a1517b24bc071a033768aa5e6d9
Robert Hull <robertthull23@gmail.com>
2018-11-18 03:08:25 -0600

    removes unused imports; adds function docstrings

--- ytDownload.py
+++ ytDownload.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
-import sys
+from concurrent.futures import ThreadPoolExecutor
 import pafy
 import click
-import threading
-from concurrent.futures import ThreadPoolExecutor
 
 @click.command()
 @click.argument('urls', nargs=-1)
 @click.option('-a', '--audio', is_flag=True)
 def download(urls, audio):
+    """Calls download_best for each video url."""
     executor = ThreadPoolExecutor(max_workers=8)
     for url in urls:
         if 'playlist' in url:
             playlist = pafy.get_playlist(url)
             for item in playlist['items']:
-                executor.submit(downloadBest, item['pafy'], audio)
+                executor.submit(download_best, item['pafy'], audio)
         else:
-            executor.submit(downloadBest, pafy.new(url), audio)
-        
-def downloadBest(meta, audio):
+            executor.submit(download_best, pafy.new(url), audio)
+
+def download_best(meta, audio):
+    """Download the specified video/audio."""
     file = meta.getbestaudio(preftype="m4a") if audio else meta.getbest()
     file.download()
     print("Finished: ", meta.title, "\t\t\t\t\t")
-    
+
 if __name__ == "__main__":
     download()
